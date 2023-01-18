7f6e53d8eff92f2629ff5572c376a59683d9c562
kishan-pj <78782776+kishan-pj@users.noreply.github.com>
2023-01-18 09:42:13 +0545

    search forms

--- src/App.js
+++ src/App.js
@@ -1,109 +1,88 @@
 import { useState, useEffect } from "react";
-
-//import Form from './components/Form';
-
 import axios from "axios";
 import "./App.css";
 import Note from "./components/Note";
 
 function App() {
   const [newNote, setNewNote] = useState("");
-  const [notes, setNotes] = useState([]);
-  const [showAll, setshowAll] = useState(true);
-
-  //use this
+  const [notes, setnotes] = useState([]);
+  const [showAll, setshowall] = useState(true);
+  const notestoshow = showAll
+    ? notes
+    : notes.filter((n) => n.important === true);
 
   useEffect(() => {
-    axios
-      .get("http://localhost:3001/notes")
-      .then((response) => {
-        console.log(response);
-        setNotes(response.data);
-      })
-      .catch((err) => console.log(err));
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log(response);
+      setnotes(response.data);
+    });
   }, []);
-
-  //or this
-  // const getNotes = async () => {
-  //   try {
-  //     let response = await anxios.get('http://localhost:3001/notes')
-  //     console.log(response.data)
-  //     setNotes(response.data)
-  //   }
-  //   catch (err) { console.log(err) }
-  // }
-  // useEffect(getNotes, [])
 
   const handleInputChange = (event) => {
     console.log(event.target.value);
     setNewNote(event.target.value);
   };
 
-  const handleadd = (event) => {
+  const HandleAdd = (event) => {
     event.preventDefault();
+    // create notes
 
-    //create a new note
     const note = {
       content: newNote,
       date: new Date().toString(),
       important: Math.random() < 0.5,
     };
-
     if (newNote !== "") {
       axios
-
         .post("http://localhost:3001/notes", note)
         .then((response) => {
           console.log(response);
-          setNotes(notes.concat(response.data));
+          setnotes(notes.concat(response.data));
+          setNewNote("");
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log(err);
+        });
     }
-
-    // if (newNote!=='')
-    // setNotes(notes.concat(note))
-    // setNewNote("")
   };
 
-  const notesShow = showAll ? notes : notes.filter((n) => n.important === true);
-
-  const deletenote = (id) => {
-    if (window.confirm(`do you want to delete ${id}`)) {
+  const setdelete = (id) => {
+    var response = window.confirm(
+      `do you really want to delete note with id ${id}`
+    );
+    if (response === true) {
       axios
         .delete(`http://localhost:3001/notes/${id}`)
         .then((response) => {
           console.log(response);
-          setNotes(notes.filter((n) => n.id !== id));
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          console.log(err);
+        });
+      setnotes(notes.filter((n) => n.id !== id));
+    } else {
+      setnotes(notes);
     }
   };
-
+  const tooglebutton = showAll ? "show impotant" : "show all";
   return (
     <>
       <h2>Notes</h2>
-
-      <button onClick={() => setshowAll(!showAll)}>
-        {showAll ? "show important" : "show all"}
-      </button>
-
-      {/* <button onClick={showImportant}>show important</button> */}
-
       <ul>
-        {notesToShow.map((note) => (
-          <li key={note.id}>
-            <Note note={note} />
-          </li>
+        {notes.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            setdelete={() => setdelete(note.id)}
+          />
         ))}
       </ul>
-
+      <button onClick={() => setshowall(!showAll)}>{tooglebutton} </button>
       <form>
         <input value={newNote} onChange={handleInputChange} />
-
-        <button onClick={handleadd}>add</button>
+        <button onClick={HandleAdd}> add </button>
       </form>
     </>
   );
 }
-
 export default App;
